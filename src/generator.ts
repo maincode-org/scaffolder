8facd837c5f5f73af47b78e342675f400201f0e0
Markido <markido@live.dk>
2021-08-01 15:23:51 +0200

    Initial refactor complete

--- src/generator.ts
+++ src/generator.ts
@@ -8,54 +8,60 @@ import pkg from 'mkdirp';
 
 const { sync } = pkg;
 
-
 export const generateCommand = async (program: Command) => {
-
-  program.command('generate').alias('gen')
-    .option('-o, --outdir <outdir>', 'relative path to output dir');
+  program.command('generate').alias('gen').option('-o, --outdir <outdir>', 'relative path to output dir');
 
   program.parse();
   const options = program.opts();
-  const fileName = program.args; // The "rest" of the arguments given. "command [options] filename".
 
-  /** Identify the correct template. */
-  const templateChoice = await prompts([
+  /** Prompt the user for template, options and filename. */
+  const answers = await prompts([
     {
-    type: 'select',
-    name: 'template',
-    message: 'Choose a code template',
-    choices: [
-      { title: 'React.js Component', value: ETemplates.component },
-      { title: 'Ionic screen', value: ETemplates.screen },
-    ],
-    instructions: false
-  },
+      type: 'select',
+      name: 'template',
+      message: 'Choose a code template',
+      choices: [
+        { title: 'React.js Component', value: ETemplates.component },
+        { title: 'Ionic screen', value: ETemplates.screen },
+      ],
+      instructions: false,
+    },
     {
-      type: prev => prev === ETemplates.component ? 'confirm' : null,
+      type: (prev) => (prev === ETemplates.component ? 'confirm' : null),
       name: 'includeCSS',
       message: 'Include a CSS module?',
-      initial: 'y'
-    }
+      initial: 'y',
+    },
+    {
+      type: 'text',
+      name: 'fileName',
+      message: 'Enter filename. Remember correct casing, but leave out the extension as it is automatically added.',
+    },
   ]);
 
-  const template: ITemplateConfig | undefined = templateConfigs.get(templateChoice.template);
-  console.log(templateChoice);
+  /** Identify the correct template. */
+  const template: ITemplateConfig | undefined = templateConfigs.get(answers.template);
+  if (!template) throw 'Template not found.';
+
+  console.log(answers);
   console.log(template);
 
-  /** Create the correct folders if needed. */
-// TODO: Make file-name dash-cased, remove ext and include a folder for it (only when component).
-// TODO: if outdir starts with ./, strip ./ from outdir.
-  /*
-  const outDir = `./${options.outdir ?? template.defaultOutDir}`;
+  /** For components, append a sub-folder with the component name in dash-case. */
+  let dashCasedFilename = answers.fileName.replace(/[A-Z]/g, (m: string) => '-' + m.toLowerCase()).substring(1);
+  const defaultOutDir = template.type === ETemplates.component ? `${template.defaultOutDir}/${dashCasedFilename}` : template.defaultOutDir;
+
+  /** Create the correct folders if needed. (remove ./ prefix when supplied from the user). */
+  const outDir = `./${options.outdir.replace('./', '') ?? defaultOutDir}`;
   sync(outDir);
 
-// TODO: Write the correct template file (from github download).
-// TODO: if not filename ends on the same as the template filename ends, append it.
-  const file = fs.createWriteStream(`${outDir}/${fileName}`);
-  const request = https.get(template.downloadURL, function (response) {
-    response.pipe(file);
-    console.log('Yay, boilerplate code written! ;)');
-  });
-*/
-// TODO: Modify it to the given params/name
-}
+  // TODO add CSS module.
+
+  /** Creates the file in the output directory, with the correct extension (user given extensions are stripped by regex). */
+  const fileStream = fs.createWriteStream(`${outDir}/${answers.fileName.replace(/\.[^/.]+$/, '')}.${template.fileExtension}`);
+
+  /** Writes the correct template file (from github download). */
+  const file = await https.get(template.downloadURL);
+  file.pipe(fileStream);
+
+  // TODO: Modify it to the given params/name
+};
--- src/template-configs.ts
+++ src/template-configs.ts
@@ -5,9 +5,9 @@ export enum ETemplates {
 
 export type ITemplateConfig = {
   type: ETemplates;
-  fileName: string;
-  downloadURL: string;
+  fileExtension: string;
   defaultOutDir: string;
+  downloadURL: string;
 };
 
 export const templateConfigs = new Map<ETemplates, ITemplateConfig>([
@@ -15,16 +15,16 @@ export const templateConfigs = new Map<ETemplates, ITemplateConfig>([
     ETemplates.component,
     {
       type: ETemplates.component,
-      fileName: 'component-boilerplate.tsx',
+      fileExtension: 'tsx',
       defaultOutDir: '/src/components',
       downloadURL: 'https://raw.githubusercontent.com/maincode-org/code-snippets/main/frontend/component-boilerplate.tsx',
     },
   ],
-  [ETemplates.screen, { type: ETemplates.screen, fileName: '', defaultOutDir: '/src/screens', downloadURL: '' }],
+  [ETemplates.screen, { type: ETemplates.screen, fileExtension: 'tsx', defaultOutDir: '/src/screens', downloadURL: '' }],
 ]);
 
 export const listTemplates = () => {
   console.log('--- Templates -----');
   Array.from(templateConfigs.keys()).forEach((template) => console.log(`> ${template}`));
   console.log('-------------------');
-}
+};
