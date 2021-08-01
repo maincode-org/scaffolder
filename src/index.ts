867f83df2ba18f9827dbbe9a7f0818c2ca0cd5ff
Markido <markido@live.dk>
2021-08-01 22:38:30 +0200

    Cleaned away the list command

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { listTemplates } from './template-configs.js';
 import { generateCommand } from './generator.js';
 
 const program = new Command();
 
-program.command('list').alias('ls').description('List all templates').action(() => listTemplates());
-
-generateCommand(program).then(() => console.log("Thank you for using Scaffolder! ʘ‿ʘ"));
+generateCommand(program).then(() => console.log('Thank you for using Scaffolder! ʘ‿ʘ'));
