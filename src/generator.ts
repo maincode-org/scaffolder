4eccf8af181195f7ce5c58e8356e002f4306bf44
Markido <markido@live.dk>
2021-08-01 18:59:23 +0200

    Cleaned up entityname and output dir

--- src/generator.ts
+++ src/generator.ts
@@ -9,14 +9,9 @@ import pkg from 'mkdirp';
 const { sync } = pkg;
 
 export const generateCommand = async (program: Command) => {
-  // TODO include custom outdir as option.
-  program.command('generate').alias('gen').option('-o, --outdir <outdir>', 'relative path to output dir');
+  program.command('generate').alias('gen').parse();
 
-  program.parse();
-  const options = program.opts();
-
-  // TODO rename filename to entityName
-  /** Prompt the user for template, options and filename. */
+  /** Prompt the user for template, options and entity name. */
   const answers = await prompts([
     {
       type: 'select',
@@ -36,8 +31,14 @@ export const generateCommand = async (program: Command) => {
     },
     {
       type: 'text',
-      name: 'fileName',
-      message: 'Enter filename. Remember correct casing, but leave out the extension as it is automatically added.',
+      name: 'entityName',
+      message: 'Enter entity name. Eg. "MyComponent". Remember correct casing, but leave out the extension as it is automatically added.',
+    },
+    {
+      type: 'text',
+      name: 'outDir',
+      initial: '',
+      message: 'Custom output directory overwrite.',
     },
   ]);
 
@@ -46,17 +47,17 @@ export const generateCommand = async (program: Command) => {
   if (!template) throw 'Template not found.';
 
   /** For components, append a sub-folder with the component name in dash-case. */
-  let dashCasedFilename = answers.fileName.replace(/[A-Z]/g, (m: string) => '-' + m.toLowerCase()).substring(1);
+  let dashCasedFilename = answers.entityName.replace(/[A-Z]/g, (m: string) => '-' + m.toLowerCase()).substring(1);
   const defaultOutDir = template.type === ETemplates.component ? `${template.defaultOutDir}/${dashCasedFilename}` : template.defaultOutDir;
 
   /** Create the correct folders if needed. (remove ./ prefix when supplied from the user). */
-  const outDir = `./${options.outdir?.replace('./', '') ?? defaultOutDir}`;
+  const outDir = `./${answers.outDir?.replace('./', '') ?? defaultOutDir}`;
   sync(outDir);
 
   // TODO add CSS module.
 
   /** Creates the file in the output directory, with the correct extension (user given extensions are stripped by regex). */
-  const fileStream = fs.createWriteStream(`${outDir}/${answers.fileName.replace(/\.[^/.]+$/, '')}.${template.fileExtension}`);
+  const fileStream = fs.createWriteStream(`${outDir}/${answers.entityName.replace(/\.[^/.]+$/, '')}.${template.fileExtension}`);
 
   /** Writes the correct template file (from github download). */
   https.get(template.downloadURL, (response) => {
@@ -66,7 +67,7 @@ export const generateCommand = async (program: Command) => {
     fs.readFile(fileStream.path, 'utf8', function (err, data) {
       if (err) return console.log(err);
 
-      let result = data.replace(/BoilerPlate/g, answers.fileName);
+      let result = data.replace(/BoilerPlate/g, answers.entityName);
 
       fs.writeFile(fileStream.path, result, 'utf8', (err) => (err ? console.log(err) : null));
     });
