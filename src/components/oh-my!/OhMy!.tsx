8633f11c41863bfe4829df0406b574e7b0afb486
Markido <markido@live.dk>
2021-08-01 18:06:38 +0200

    Works, no CSS modules tho

---
+++ src/components/oh-my!/OhMy!.tsx
@@ -0,0 +1,12 @@
+/** Describe the purpose of the OhMy! component here.*/
+
+type IProps = {
+    dummy: string;
+}
+
+const OhMy!: React.FC<IProps> = ({ dummy }) => {
+    return (
+      <div></div>
+    );
+}
+export default OhMy!;
--- src/generator.ts
+++ src/generator.ts
@@ -9,11 +9,13 @@ import pkg from 'mkdirp';
 const { sync } = pkg;
 
 export const generateCommand = async (program: Command) => {
+  // TODO include custom outdir as option.
   program.command('generate').alias('gen').option('-o, --outdir <outdir>', 'relative path to output dir');
 
   program.parse();
   const options = program.opts();
 
+  // TODO rename filename to entityName
   /** Prompt the user for template, options and filename. */
   const answers = await prompts([
     {
@@ -43,20 +45,13 @@ export const generateCommand = async (program: Command) => {
   const template: ITemplateConfig | undefined = templateConfigs.get(answers.template);
   if (!template) throw 'Template not found.';
 
-  console.log(answers);
-  console.log(template);
-
   /** For components, append a sub-folder with the component name in dash-case. */
   let dashCasedFilename = answers.fileName.replace(/[A-Z]/g, (m: string) => '-' + m.toLowerCase()).substring(1);
   const defaultOutDir = template.type === ETemplates.component ? `${template.defaultOutDir}/${dashCasedFilename}` : template.defaultOutDir;
 
-  console.log('Prepared default output folder');
-
   /** Create the correct folders if needed. (remove ./ prefix when supplied from the user). */
   const outDir = `./${options.outdir?.replace('./', '') ?? defaultOutDir}`;
   sync(outDir);
-
-  console.log('Made output folder');
 
   // TODO add CSS module.
 
@@ -64,7 +59,16 @@ export const generateCommand = async (program: Command) => {
   const fileStream = fs.createWriteStream(`${outDir}/${answers.fileName.replace(/\.[^/.]+$/, '')}.${template.fileExtension}`);
 
   /** Writes the correct template file (from github download). */
-  const response = https.get(template.downloadURL, (response) => response.pipe(fileStream));
+  https.get(template.downloadURL, (response) => {
+    response.pipe(fileStream);
 
-  // TODO: Modify it to the given params/name
+    /** Replace the keyword "BoilerPlate" with the entity name in the file. */
+    fs.readFile(fileStream.path, 'utf8', function (err, data) {
+      if (err) return console.log(err);
+
+      let result = data.replace(/BoilerPlate/g, answers.fileName);
+
+      fs.writeFile(fileStream.path, result, 'utf8', (err) => (err ? console.log(err) : null));
+    });
+  });
 };
